0cace45adf0429750e65eacb7bf82aff59e0d8d1
Meheer007 <>
2021-10-14 12:23:29 +0000

    53%

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const passport = require('passport');
 const ObjectID = require('mongodb').ObjectID;
 const app = express();
 const LocalStrategy = require('passport-local');
+const bcrypt = require('bcrypt');
 
 app.set('view engine', 'pug')
 
@@ -34,36 +35,90 @@ myDB(async (client) => {
     app.route('/').get((req, res) => {
         res.render('pug', {
             title: 'Connected to Database',
-            message: 'Please login'
+            message: 'Please login',
+            showLogin: true,
+            showRegistration: true
         });
+    });
+
+    app.route('/login').post(passport.authenticate('local', { failureRedirect: '/' }), (req, res) => {
+        res.redirect('/profile');
+    });
+
+    app.route('/profile').get(ensureAuthenticated, (req, res) => {
+        res.render(process.cwd() + '/views/pug/profile', {username: req.user.username});
+    });
+
+    app.route('/logout').get((req, res) => {
+        req.logout();
+        res.redirect('/');
+    });
+
+    app.use((req, res, next) => {
+        res.status(404)
+        .type('text')
+        .send('Not Found')
+    })
+
+    app.route('/register').post((req, res, next) => {
+        const hash = bcrypt.hashSync(req.body.password, 12);
+        adn.findOne({username: req.body.username}, (err, user) => {
+            if(err){
+                next(err);
+            } else if(user) {
+                res.redirect('/');
+            } else {
+                adn.insertOne({
+                    username: req.body.username,
+                    password: hash
+                }, (err, doc) => {
+                    if (err){
+                        res.redirect('/');
+                    } else {
+                        next(null, doc.ops[0]);
+                    }
+                })
+            }
+        })
+    },
+    passport.authenticate('local', {failureRedirect: '/'}), (req, res, next) => {
+        res.redirect('/profile');
     });
 
     passport.serializeUser((user, done) => {
         done(null, user._id);
     });
+
     passport.deserializeUser((id, done) => {
-        adn.findOne({_id: new ObjectID(id)}, (err, doc) => {
+        adn.findOne({ _id: new ObjectID(id) }, (err, doc) => {
             done(null, doc);
         });
-    });
-    
+    });   
+
     passport.use(new LocalStrategy(
         (username, password, done) => {
-            adn.findOne({usernmae: username}, (err, user) => {
+            adn.findOne({username: username}, (err, user) => {
                 console.log("User " + username + " attempted to login");
                 if (err) {return done(err); }
                 if (!user) {return done(null, false);}
-                if (password !== user.password) {return done(null, false);}
+                if (!bcrypt.compareSync(password, user.password)) {return done(null, false);}
                 return done(null, user)
             });
         }
     ));
 
-}).catch(e => {
+}).catch((e) => {
     app.route('/').get((req, res) => {
         res.render("pug", {title: e, message: "unable to login"} )
-    })
-})
+    });
+});
+
+function ensureAuthenticated(req, res, next) {
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect('/');
+}
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
