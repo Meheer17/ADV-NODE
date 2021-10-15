6daf4ef01c3d96ed86f43e2543f0825a896e4738
Meheer007 <>
2021-10-15 13:36:03 +0000

    error

--- server.js
+++ server.js
@@ -2,57 +2,87 @@
 require('dotenv').config();
 const express = require('express');
 const myDB = require('./connection');
-const passport = require('passport');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
 const session = require('express-session');
-const app = express();
+const passport = require('passport');
 const routes = require('./routes');
 const auth = require('./auth.js');
-const LocalStrategy = require('passport-local');
-const bcrypt = require('bcrypt');
+
+const app = express();
 const http = require('http').createServer(app);
-const io = require('socket.io')(http)
+const io = require('socket.io')(http);
+const passportSocketIo = require('passport.socketio');
+const cookieParser = require('cookie-parser');
+const MongoStore = require('connect-mongo')(session);
+const URI = process.env.MONGO_URI;
+const store = new MongoStore({ url: URI });
 
-app.set('view engine', 'pug')
+app.set('view engine', 'pug');
 
-fccTesting(app); //For FCC testing purpos
-
+fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 app.use(session({
-    secret: process.env.SESSION_SECRET,
-    resave: true,
-    saveUninitialized: true,
-    cookie: {secure: false}  
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  cookie: { secure: false },
+  key: 'express.sid',
+  store: store
 }));
+
 app.use(passport.initialize());
 app.use(passport.session());
 
+io.use(
+  passportSocketIo.authorize({
+    cookieParser: cookieParser,
+    key: 'express.sid',
+    secret: process.env.SESSION_SECRET,
+    store: store,
+    success: onAuthorizeSuccess,
+    fail: onAuthorizeFail
+  })
+);
+
 myDB(async (client) => {
-    const adn = await client.db('database').collection('users');
+  const myDataBase = await client.db('database').collection('users');
 
-    routes(app, adn);
-    auth(app, adn);
+  routes(app, myDataBase);
+  auth(app, myDataBase);
 
-    io.on('connection', socket => {
-        console.log('A user has connected')
+  let currentUsers = 0;
+  io.on('connection', (socket) => {
+    ++currentUsers;
+    io.emit('user count', currentUsers);
+    console.log('user ' + socket.request.user.username + ' connected');
+
+    socket.on('disconnect', () => {
+      console.log('A user has disconnected');
+      --currentUsers;
+      io.emit('user count', currentUsers);
     });
-
+  });
 }).catch((e) => {
-    app.route('/').get((req, res) => {
-        res.render("pug", {title: e, message: "unable to login"} )
-    });
+  app.route('/').get((req, res) => {
+    res.render('pug', { title: e, message: 'Unable to login' });
+  });
 });
 
-function ensureAuthenticated(req, res, next) {
-    if(req.isAuthenticated()){
-        return next();
-    }
-    res.redirect('/');
-};
+function onAuthorizeSuccess(data, accept) {
+  console.log('successful connection to socket.io');
 
-const PORT = process.env.PORT || 3000;
-http.listen(PORT, () => {
-  console.log('Listening on port ' + PORT);
+  accept(null, true);
+}
+
+function onAuthorizeFail(data, message, error, accept) {
+  if (error) throw new Error(message);
+  console.log('failed connection to socket.io:', message);
+  accept(null, false);
+}
+
+http.listen(process.env.PORT || 3000, () => {
+  console.log('Listening on port ' + process.env.PORT);
 });
