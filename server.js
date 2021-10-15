81774b3235ee0b5a129c527ad9d6df226bb57f28
Meheer007 <>
2021-10-15 10:18:48 +0000

    73%

--- server.js
+++ server.js
@@ -1,20 +1,18 @@
-// all the const 
 'use strict';
 require('dotenv').config();
 const express = require('express');
 const myDB = require('./connection');
+const passport = require('passport');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
 const session = require('express-session');
-const passport = require('passport');
-const ObjectID = require('mongodb').ObjectID;
 const app = express();
-const LocalStrategy = require('passport-local');
+const routes = require('./routes');
+const auth = require('./auth.js');
 
 app.set('view engine', 'pug')
 
-fccTesting(app); //For FCC testing purposes
+fccTesting(app); //For FCC testing purpos
 
-// app.use 
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -27,43 +25,17 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-// routes
 myDB(async (client) => {
     const adn = await client.db('database').collection('users');
 
-    app.route('/').get((req, res) => {
-        res.render('pug', {
-            title: 'Connected to Database',
-            message: 'Please login'
-        });
-    });
+    routes(app, adn);
+    auth(app, adn);
 
-    passport.serializeUser((user, done) => {
-        done(null, user._id);
-    });
-    passport.deserializeUser((id, done) => {
-        adn.findOne({_id: new ObjectID(id)}, (err, doc) => {
-            done(null, doc);
-        });
-    });
-    
-    passport.use(new LocalStrategy(
-        (username, password, done) => {
-            adn.findOne({usernmae: username}, (err, user) => {
-                console.log("User " + username + " attempted to login");
-                if (err) {return done(err); }
-                if (!user) {return done(null, false);}
-                if (password !== user.password) {return done(null, false);}
-                return done(null, user)
-            });
-        }
-    ));
-
-}).catch(e => {
+}).catch((e) => {
     app.route('/').get((req, res) => {
         res.render("pug", {title: e, message: "unable to login"} )
-    })
-})
+    });
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
